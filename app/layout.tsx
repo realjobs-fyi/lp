aa070c2d5d70ed51373e73c643b715e0619bb080
Marcelo Lopes <marcelo.lopes2001@hotmail.com>
2026-01-23 14:44:02 -0500

    chore: remove @vercel/analytics dependency and related component from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -60,7 +59,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Analytics />
       </body>
     </html>
   );
